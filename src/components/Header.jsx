dee969295be39a6d9e98e0c12d251b15b0eccd7c
Andy Yun <andy000923@gmail.com>
2023-01-31 00:24:49 +0900

    new

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import "./Header.css";
 import expandMore1 from "../assets/expandMore1.svg";
 import kakaoTalkPhoto2023 from "../assets/kakaoTalkPhoto2023.png";
-<<<<<<< HEAD
 import univcertlogo from "../assets/UNIVCERT.png"
 const Header = (props) => {
   return (
@@ -18,23 +17,6 @@ const Header = (props) => {
           <button className="frame-31">API 키 발급</button>
           </div>
       </div>
-=======
-const Header = (props) => {
-  return (
-    <div className='header-container'>
-      <img className="kakao-talk-photo-2023" src={kakaoTalkPhoto2023} />
-
-      <span className="apis">{props.apIs || "APIs"}</span>
-      <img className="expand-more-1" src={props.expandMore1 || expandMore1} />
-      
-      <span className="documentation">{props.documentation || "Documentation"}</span>
-
-      <span className="pricing">{props.pricing || "Pricing"}</span>
-
-      <span className="login">{props.login || "Login"}</span>
-
-      <button className="signup-button">{props.signUp || "SIGN UP"}</button>
->>>>>>> main
     </div>
   );
 };
