6996eb25450371c57c6baca306350deb4e162b12
Yun Kanghyun <yunkanghyun@Yunui-MacBookPro.local>
2023-02-09 20:37:55 +0900

    final

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,10 +29,13 @@ const Home = () => {
         e.preventDefault();
         setemail(e.target.value);
     }
-
+    const handleTest = (e)=>{
+      alert("대학명 체크를 먼저 진행해 주세요.");
+  }
     const handleChange_univName = (e)=>{
         e.preventDefault();
         setunivName(e.target.value);
+        setunivcheck(false);
     }
     const handleCheckUniv = async (e) => {
       e.preventDefault();
@@ -85,7 +88,7 @@ const Home = () => {
                   setcolor(false);
                 }
                 else if (response.data.status === 400) {
-                  alert("Bad Request , expected : "+(response.data.expected));
+                  alert("잘못된 요청입니다, 예상 대학 도메인 : "+(response.data.expected));
                   setsuccess("false");
                   setcolor(true);
                 }
@@ -98,7 +101,7 @@ const Home = () => {
             (error)=>{
                 console.log(error); 
               if (error.code != null) {
-                    alert(error.message);
+                    alert("잘못된 메일 형식입니다. 예시 : abc123@snu.ac.kr");
                     setcolor(true);
                 }  
             });
@@ -111,62 +114,70 @@ const Home = () => {
         
           <div className="flex-container">
             <span className="email-validation-and">
-            <br/> 메일(+대학) 인증 API
+            <h1 className='titletext'>메일<small className='plustext'> </small>(<small className='plustext'> + </small>대학<small className='plustext'> </small>) 인증 API</h1>
             </span>
             <span className="improve-your-deliver">
-            <br/> 단 <span class="hljs-pink">한 줄</span>의 코드로 <br/> 사용자의 메일과 대학 재학 여부까지 인증해 보세요.
+            <br/> <h1 className='oneline'> 단 <span className="hljs-pink">한 줄</span>의 코드로 .<br/> 사용자의 메일과 대학 재학 여부까지 인증해 보세요. </h1>
             </span>
 
             {univcheck? 
             <div className="univName-container">
               <input type="text" className="univName-input" required={true} value={univName} onChange={handleChange_univName} placeholder=" ex) 홍익대학교"/>
-              <button type="submit" className="test-button" onClick={handleCheckUniv}><img className='checkimg' src={checkimg}/></button>
+              <button type="submit" className="test-button" onClick={handleCheckUniv}><img className='checkimg' alt="checkimg" src={checkimg}/></button>
             </div> 
             :
             <div className="univName-container">
               <input type="text" className="univName-input" required={true} value={univName} onChange={handleChange_univName} placeholder=" ex) 홍익대학교"/>
-              <button type="submit" className="test-button" onClick={handleCheckUniv}>대학명 체크</button>
+              <button type="submit" className="test-button" onClick={handleCheckUniv}><span className="hljs-white">대학명 체크</span></button>
             </div> 
             }
 
+            {univcheck? 
             <div className="email-container">
               <input type="email" className="email-input" required={true} value={email} onChange={handleChange_email} placeholder=" ex) abc123@mail.hongik.ac.kr"/>
-              <button type="submit" className="test-button" onClick={handleSubmit}>테스트</button>
+              <button type="submit" className="test-button" onClick={handleSubmit}><span className="hljs-white">테스트</span></button>
             </div> 
+            :
+            <div className="email-container">
+              <input type="email" className="email-input" required={true} value={email} onChange={handleChange_email} placeholder=" ex) abc123@mail.hongik.ac.kr"/>
+              <button type="submit" className="test-button" onClick={handleTest}><span className="hljs-white">테스트</span></button>
+            </div> 
+            }
+            
             <div className="checkbox-container">
-              <img src={checkbox} className="checkbox"/>
-              <ul class="checkbox-content">
-                <p className='check-text'>메일만 인증하는 <br/> 테스트를 위해서는 <br/> APIs 페이지를 <br/> 참고해주세요.</p>
+              <img src={checkbox} alt="checkbox" className="checkbox"/>
+              <ul className="checkbox-content">
+                <p className='check-text'>대학인증이 불필요하신 분은<br/>APIs 페이지를 참고해주세요.</p>
               </ul>
             </div>
           </div>
           <div className="flex-container-div"></div>
           <div className="flex-container-1">
-            <div class="w-full lg:w-1/3">
+            <div className="w-full lg:w-1/3">
               <div className="info-container">
-                UnivCert.certify("private Key", {email}, {univName}, true);
+                &nbsp;&nbsp;UnivCert.certify("private Key", {email1}, {univName1}, true);
               </div>
-              <div class="code-container">
-                <pre class="flex items-center h-64 rounded shadow-xl ">                        
+              <div className="code-container">
+                <pre className="flex items-center h-64 rounded shadow-xl ">                        
                   {color?
-                    <code class="language-json hljs">
-                      <span class="hljs-punctuation">&#123; </span><br/>
-                          <span class="hljs-red"> "status"</span><span class="hljs-punctuation">:</span> <span class="hljs-number">{status}</span><span class="hljs-punctuation">,</span><br/>
-                          <span class="hljs-red"> "domain"</span><span class="hljs-punctuation">:</span> <span class="hljs-string">{domain}</span><span class="hljs-punctuation">,</span> <br/>
-                          <span class="hljs-red"> "univ_email"</span><span class="hljs-punctuation">:</span> <span class="hljs-keyword">{email1}</span><span class="hljs-punctuation">,</span><br/>
-                          <span class="hljs-red"> "univ_name"</span><span class="hljs-punctuation">:</span> <span class="hljs-keyword">{univName1}</span><span class="hljs-punctuation">,</span><br/>
-                          <span class="hljs-red"> "success"</span><span class="hljs-punctuation">:</span> <span class="hljs-purple">{success}</span><br/>
-                      <span class="hljs-punctuation">&#125; </span>
+                    <code className="language-json hljs">
+                      <span className="hljs-punctuation">&#123; </span><br/>
+                          <span className="hljs-red">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"status"</span> <span className="hljs-punctuation">:</span> <span className="hljs-number">{status}</span><span className="hljs-punctuation">,</span><br/>
+                          <span className="hljs-red">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"domain"</span> <span className="hljs-punctuation">:</span> <span className="hljs-string">{domain}</span><span className="hljs-punctuation">,</span> <br/>
+                          <span className="hljs-red">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"univ_email"</span> <span className="hljs-punctuation">:</span> <span className="hljs-keyword">{email1}</span><span className="hljs-punctuation">,</span><br/>
+                          <span className="hljs-red">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"univ_name"</span> <span className="hljs-punctuation">:</span> <span className="hljs-keyword">{univName1}</span><span className="hljs-punctuation">,</span><br/>
+                          <span className="hljs-red">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"success"</span> <span className="hljs-punctuation">:</span> <span className="hljs-purple">{success}</span><br/>
+                      <span className="hljs-punctuation">&#125; </span>
                     </code>
                     :
-                    <code class="language-json hljs">
-                      <span class="hljs-punctuation">&#123; </span><br/>
-                          <span class="hljs-green"> "status"</span><span class="hljs-punctuation">:</span> <span class="hljs-number">{status}</span><span class="hljs-punctuation">,</span><br/>
-                          <span class="hljs-green"> "domain"</span><span class="hljs-punctuation">:</span> <span class="hljs-string">{domain}</span><span class="hljs-punctuation">,</span> <br/>
-                          <span class="hljs-green"> "univ_email"</span><span class="hljs-punctuation">:</span> <span class="hljs-keyword">{email1}</span><span class="hljs-punctuation">,</span><br/>
-                          <span class="hljs-green"> "univ_name"</span><span class="hljs-punctuation">:</span> <span class="hljs-keyword">{univName1}</span><span class="hljs-punctuation">,</span><br/>
-                          <span class="hljs-green"> "success"</span><span class="hljs-punctuation">:</span> <span class="hljs-purple">{success}</span><br/>
-                      <span class="hljs-punctuation">&#125; </span>
+                    <code className="language-json hljs">
+                      <span className="hljs-punctuation">&#123; </span><br/>
+                          <span className="hljs-green">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"status"</span> <span className="hljs-punctuation">:</span> <span className="hljs-number">{status}</span><span className="hljs-punctuation">,</span><br/>
+                          <span className="hljs-green">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"domain"</span> <span className="hljs-punctuation">:</span> <span className="hljs-string">{domain}</span><span className="hljs-punctuation">,</span> <br/>
+                          <span className="hljs-green">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"univ_email"</span> <span className="hljs-punctuation">:</span> <span className="hljs-keyword">{email1}</span><span className="hljs-punctuation">,</span><br/>
+                          <span className="hljs-green">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"univ_name"</span> <span className="hljs-punctuation">:</span> <span className="hljs-keyword">{univName1}</span><span className="hljs-punctuation">,</span><br/>
+                          <span className="hljs-green">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"success"</span> <span className="hljs-punctuation">:</span> <span className="hljs-purple">{success}</span><br/>
+                      <span className="hljs-punctuation">&#125; </span>
                     </code>
                   }
                   
@@ -178,7 +189,7 @@ const Home = () => {
       </div>
       <div className='footer-container'>
         <div className="flex-container-2">
-          <img className="group170" src={group170} />
+          <img className="group170" alt="" src={group170} />
         </div>
         <ContactUsButton/>
       </div>
