cd4c22aded5a0f577f6b357ea59b2090334b5891
Yun Kanghyun <yunkanghyun@Yunui-MacBookPro.local>
2023-02-09 21:10:15 +0900

    opengraph

---
+++ src/components/Opengraphimg.jsx
@@ -0,0 +1,8 @@
+import opengraphimg from "../assets/opengraph.svg";
+
+const Opengraphimg = () => {
+  return (
+    <img src={opengraphimg} alt={`opengraph`} />
+  );
+};
+export default Opengraphimg;
