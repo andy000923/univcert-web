6c01099a43f33dd548f5e25e0d26e16538618e27
in-seo <sis000512@gmail.com>
2023-02-10 20:29:56 +0900

    fix: contactusButton

--- src/components/ContactUsButton.jsx
+++ src/components/ContactUsButton.jsx
@@ -4,7 +4,9 @@ import image9 from "../assets/image9.svg";
 const ContactUsButton = (props) => {
   return (
     <div className="flex-container-5">
-      <img className="image9" src={image9} />
+        <a href="https://github.com/in-seo/univcert" target='_blank'>
+            <img className="image9" src={image9} />
+        </a>
       <a href="https://fork-player-abb.notion.site/63617df354c042c39382d7f2d9a9bab9" target='_blank'>
         <img className="Button9" src={Button9} />
       </a>
