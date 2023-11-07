1ec4b8dd0f0ec2e10ed8a505305845b66b24e41d
Andy Yun <andy000923@gmail.com>
2023-11-07 22:04:53 +0900

    드롭다운 크기 변경

--- src/pages/information/Info3.jsx
+++ src/pages/information/Info3.jsx
@@ -10,21 +10,9 @@ const Info3= () => {
       <Header/>
 
       <div className="container">
-        <div className="frame-container">
-          <Link to="/instruction" style={{ textDecoration: "none" , color:"black"}}> <img className="Frame" src={Frame1}/></Link>
-          <img className="Fmain" src={Fmain} />
-          <Link to="/instruction1" style={{ textDecoration: "none" , color:"black"}}> <img className="Frame" alt="이용자 메일 인증 시작" src={Frame2}/></Link>
-          <Link to="/instruction2" style={{ textDecoration: "none" , color:"black"}}><img className="Frame" alt="발송된 인증코드 입력" src={Frame3}/></Link>
-          <img className="Fexport" src={Fexport}/>
-          <Link to="/instruction3" style={{ textDecoration: "none" , color:"black"}}><img className="Frame" alt="인증된 이메일인지 확인" src={Frame4}/></Link>
-          <Link to="/instruction4" style={{ textDecoration: "none" , color:"black"}}><img className="Frame" alt="인증된 유저 리스트 출력" src={Frame5}/></Link>
-          <Link to="/instruction5" style={{ textDecoration: "none" , color:"black"}}><img className="Frame" alt="대학명 체크" src={Frame7}/></Link>
-          <a href="https://incredible-raincoat-c52.notion.site/cdb55dd7ea5b475eaaf94ce76fa9eba4" target="_blank">
-            <img className="Frame" src={Frame6} />
-          </a>
-        </div>
+      <InfoBar/>
         <div className="img-container">
-            <img className="FrameImg" src={f3} />
+            <img className="FrameImg" alt="" src={f3} />
         </div>
         <div className="contact-container">
             <ContactUsButton/>
