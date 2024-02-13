a425f5b83ef51eb2335a02a2aa80388a8d947031
Andy Yun <andy000923@gmail.com>
2024-02-13 11:47:05 +0900

    fix:[24.02.13]홈화면 애니메이션 추가

--- src/components/ContactUsButton.jsx
+++ src/components/ContactUsButton.jsx
@@ -4,11 +4,11 @@ import image9 from "../assets/image9.svg";
 const ContactUsButton = (props) => {
   return (
     <div className="flex-container-5">
-        <a href="https://github.com/in-seo/univcert" target='_blank'>
-            <img className="image9" src={image9} />
-        </a>
-      <a href="https://fork-player-abb.notion.site/63617df354c042c39382d7f2d9a9bab9" target='_blank'>
-        <img className="Button9" src={Button9} />
+      <a href="https://github.com/in-seo/univcert">
+        <img className="image9" alt="github" src={image9} />
+      </a>
+      <a href="https://fork-player-abb.notion.site/63617df354c042c39382d7f2d9a9bab9">
+        <img className="Button9" alt="notion" src={Button9} />
       </a>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,6 +26,8 @@ const Home = () => {
     const [univName1, setunivName1] = useState();
     const [status, setstatus] = useState([]);
     const [success, setsuccess] = useState([]);
+    const [animationActive, setAnimationActive] = useState(false);
+
     useEffect(() => {
       setstatus("200"); 
       setsuccess("true"); 
@@ -34,90 +36,94 @@ const Home = () => {
       setunivName1("홍익대학교");
     }, []);
     const handleChange_email = (e)=>{
-        e.preventDefault();
-        setemail(e.target.value);
+      e.preventDefault();
+      setemail(e.target.value);
     }
     const handleTest = (e)=>{
       alert("대학명 체크를 먼저 진행해 주세요.");
-  }
+    }
     const handleChange_univName = (e)=>{
-        e.preventDefault();
-        setunivName(e.target.value);
-        setunivcheck(false);
+      e.preventDefault();
+      setunivName(e.target.value);
+      setunivcheck(false);
     }
     const handleCheckUniv = async (e) => {
       e.preventDefault();
       axios.defaults.withCredentials = true;
       await axios
-          .post(baseUrl + "/checkuniv", {
+        .post(baseUrl + "/checkuniv", {
+          univName:univName,
+        },
+        {
+            withCredentials: true // 쿠키 cors 통신 설정
+          })
+        .then(response=>{
+          console.log(response);
+          if (response.data.code == null) {          
+            alert("테스트가 가능한 학교명입니다!"); 
+            setunivcheck(true);   
+          }
+          else if (response.data.code != null) {
+            alert((response.data.message));
+          }
+        },
+        (error)=>{
+            console.log(error); 
+          if (error.code != null) {
+                alert(error.message);
+            }  
+        }
+      );
+    }
+
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      axios.defaults.withCredentials = true;
+      await axios
+        .post(baseUrl + "/try", 
+          {
+            email:email,
             univName:univName,
           },
           {
-              withCredentials: true // 쿠키 cors 통신 설정
-            })
-          .then(response=>{
-            console.log(response);
-            if (response.data.code == null) {          
-              alert("테스트가 가능한 학교명입니다!"); 
-              setunivcheck(true);   
-            }
-            else if (response.data.code != null) {
-              alert((response.data.message));
-            }
-          },
-          (error)=>{
-              console.log(error); 
-            if (error.code != null) {
-                  alert(error.message);
-              }  
-          });
+            withCredentials: true // 쿠키 cors 통신 설정
+          }
+        )
+        .then(response=>{
+          console.log(response);   
+          setstatus(response.data.status); 
+          setdomain(response.data.domain);
+          setemail1(email);
+          setunivName1(univName);
+          var message = response.data.message;
+          if (response.data.status === 200) {              
+            setsuccess("true");
+            setcolor(false);
+            message = "success";
+          }
+          else if (response.data.status === 400) {
+            setsuccess("false");
+            setcolor(true);
+            message = "잘못된 요청입니다, 예상 대학 도메인 : "+ response.data.expected;
+          }
+          else if (response.data.code != null) {
+            setsuccess("false");
+            setcolor(true);
+          }
+          setTimeout(function(){
+              alert(message);
+          }, 200);
+        },
+        (error)=>{
+            console.log(error); 
+          if (error.code != null) {
+            alert("잘못된 메일 형식입니다. 예시 : abc123@snu.ac.kr");
+            setcolor(true);
+          }  
         }
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        axios.defaults.withCredentials = true;
-        await axios
-            .post(baseUrl + "/try", {
-                email:email,
-                univName:univName,
-            },
-            {
-                withCredentials: true // 쿠키 cors 통신 설정
-              })
-            .then(response=>{
-                console.log(response);   
-                setstatus(response.data.status); 
-                setdomain(response.data.domain);
-                setemail1(email);
-                setunivName1(univName);
-                var message = response.data.message;
-                if (response.data.status === 200) {              
-                  setsuccess("true");
-                  setcolor(false);
-                  message = "success";
-                }
-                else if (response.data.status === 400) {
-                  setsuccess("false");
-                  setcolor(true);
-                  message = "잘못된 요청입니다, 예상 대학 도메인 : "+ response.data.expected;
-                }
-                else if (response.data.code != null) {
-                  setsuccess("false");
-                  setcolor(true);
-                }
-                setTimeout(function(){
-                    alert(message);
-                }, 200);
-            },
-            (error)=>{
-                console.log(error); 
-              if (error.code != null) {
-                    alert("잘못된 메일 형식입니다. 예시 : abc123@snu.ac.kr");
-                    setcolor(true);
-                }  
-            });
+      );
+      setAnimationActive(!animationActive);
     }
-  
   return (
     <div className="slide-169-1">
       <div className="width-holder1">
@@ -130,7 +136,7 @@ const Home = () => {
             <h1 className='titletext'>메일<small className='plustext'></small> (<small className='plustext'>+</small>대학<small className='plustext'></small>) 인증 API</h1>
             </span>
             <span className="improve-your-deliver">
-            <h1 className='oneline'> 단 <span className="hljs-pink">한 줄</span>의 코드로 .</h1>
+            <h1 className='oneline'> 단 <span className="status-box-pink">한 줄</span>의 코드로 .</h1>
             <h2 className='secondline'>사용자의 메일과 대학 재학 여부까지 인증해 보세요.</h2>
             </span>
 
@@ -142,19 +148,19 @@ const Home = () => {
             :
             <div className="univName-container">
               <input type="text" className="univName-input" required={true} value={univName} onChange={handleChange_univName} placeholder=" ex) 홍익대학교"/>
-              <button type="submit" className="test-button" onClick={handleCheckUniv}><span className="hljs-white">대학명체크</span></button>
+              <button type="submit" className="test-button" onClick={handleCheckUniv}><span className="status-box-white">대학명체크</span></button>
             </div> 
             }
 
             {univcheck? 
             <div className="email-container">
               <input type="email" className="email-input" required={true} value={email} onChange={handleChange_email} placeholder=" ex) abc123@mail.hongik.ac.kr"/>
-              <button type="submit" className="test-button" onClick={handleSubmit}><span className="hljs-white">테스트</span></button>
+              <button type="submit" className="test-button" onClick={handleSubmit}><span className="status-box-white">테스트</span></button>
             </div> 
             :
             <div className="email-container">
               <input type="email" className="email-input" required={true} value={email} onChange={handleChange_email} placeholder=" ex) abc123@mail.hongik.ac.kr"/>
-              <button type="submit" className="test-button" onClick={handleTest}><span className="hljs-white">테스트</span></button>
+              <button type="submit" className="test-button" onClick={handleTest}><span className="status-box-white">테스트</span></button>
             </div> 
             }
             
@@ -168,36 +174,36 @@ const Home = () => {
           <div className="flex-container-div"></div>
           <div className="flex-container-1">
             <div className="w-full lg:w-1/3">
-              <div className="info-container">
+              <div className={`info-container ${animationActive ? 'animate' : ''}`}>
                 &nbsp;UnivCert.certify("private Key", {email1}, {univName1}, true);
               </div>
-              <div className="code-container">
+              <div className={`code-container ${animationActive ? 'animate' : ''}`}>
                 <pre className="flex items-center h-64 rounded shadow-xl ">                        
                   {color?
-                    <code className="language-json hljs">
-                      <span className="hljs-punctuation">&#123; </span><br/>
-                          <span className="hljs-red">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"status"</span> <span className="hljs-punctuation">:</span> <span className="hljs-number">{status}</span><span className="hljs-punctuation">,</span><br/>
-                          <span className="hljs-red">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"domain"</span> <span className="hljs-punctuation">:</span> <span className="hljs-string">{domain}</span><span className="hljs-punctuation">,</span> <br/>
-                          <span className="hljs-red">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"univ_email"</span> <span className="hljs-punctuation">:</span> <span className="hljs-keyword">{email1}</span><span className="hljs-punctuation">,</span><br/>
-                          <span className="hljs-red">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"univ_name"</span> <span className="hljs-punctuation">:</span> <span className="hljs-keyword">{univName1}</span><span className="hljs-punctuation">,</span><br/>
-                          <span className="hljs-red">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"success"</span> <span className="hljs-punctuation">:</span> <span className="hljs-purple">{success}</span><br/>
-                      <span className="hljs-punctuation">&#125; </span>
+                    <code className="status-box">
+                      <span className="status-box-punctuation">&#123; </span><br/>
+                          <span className="status-box-red">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"status"</span> <span className="status-box-punctuation">:</span> <span className="status-box-number">{status}</span><span className="status-box-punctuation">,</span><br/>
+                          <span className="status-box-red">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"domain"</span> <span className="status-box-punctuation">:</span> <span className="status-box-string">{domain}</span><span className="status-box-punctuation">,</span> <br/>
+                          <span className="status-box-red">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"univ_email"</span> <span className="status-box-punctuation">:</span> <span className="status-box-keyword">{email1}</span><span className="status-box-punctuation">,</span><br/>
+                          <span className="status-box-red">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"univ_name"</span> <span className="status-box-punctuation">:</span> <span className="status-box-keyword">{univName1}</span><span className="status-box-punctuation">,</span><br/>
+                          <span className="status-box-red">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"success"</span> <span className="status-box-punctuation">:</span> <span className="status-box-purple">{success}</span><br/>
+                      <span className="status-box-punctuation">&#125; </span>
                     </code>
                     :
-                    <code className="language-json hljs">
-                      <span className="hljs-punctuation">&#123; </span><br/>
-                          <span className="hljs-green">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"status"</span> <span className="hljs-punctuation">:</span> <span className="hljs-number">{status}</span><span className="hljs-punctuation">,</span><br/>
-                          <span className="hljs-green">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"domain"</span> <span className="hljs-punctuation">:</span> <span className="hljs-string">{domain}</span><span className="hljs-punctuation">,</span> <br/>
-                          <span className="hljs-green">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"univ_email"</span> <span className="hljs-punctuation">:</span> <span className="hljs-keyword">{email1}</span><span className="hljs-punctuation">,</span><br/>
-                          <span className="hljs-green">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"univ_name"</span> <span className="hljs-punctuation">:</span> <span className="hljs-keyword">{univName1}</span><span className="hljs-punctuation">,</span><br/>
-                          <span className="hljs-green">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"success"</span> <span className="hljs-punctuation">:</span> <span className="hljs-purple">{success}</span><br/>
-                      <span className="hljs-punctuation">&#125; </span>
+                    <code className="status-box">
+                      <span className="status-box-punctuation">&#123; </span><br/>
+                          <span className="status-box-green">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"status"</span> <span className="status-box-punctuation">:</span> <span className="status-box-number">{status}</span><span className="status-box-punctuation">,</span><br/>
+                          <span className="status-box-green">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"domain"</span> <span className="status-box-punctuation">:</span> <span className="status-box-string">{domain}</span><span className="status-box-punctuation">,</span> <br/>
+                          <span className="status-box-green">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"univ_email"</span> <span className="status-box-punctuation">:</span> <span className="status-box-keyword">{email1}</span><span className="status-box-punctuation">,</span><br/>
+                          <span className="status-box-green">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"univ_name"</span> <span className="status-box-punctuation">:</span> <span className="status-box-keyword">{univName1}</span><span className="status-box-punctuation">,</span><br/>
+                          <span className="status-box-green">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"success"</span> <span className="status-box-punctuation">:</span> <span className="status-box-purple">{success}</span><br/>
+                      <span className="status-box-punctuation">&#125; </span>
                     </code>
                   }
                 </pre>
               </div>
               <div className="info-example">
-                <span className="hljs-info">* 상단의 요청 및 응답은 예시입니다. 실제 API 사용은 APIs 페이지를 참고해주세요</span>
+                <span className="status-box-info">* 상단의 요청 및 응답은 예시입니다. 실제 API 사용은 APIs 페이지를 참고해주세요</span>
               </div>
             </div>        
           </div>
