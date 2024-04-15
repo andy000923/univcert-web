c64677039f9c6b0db1149f8c20c5fa7da3ca620b
Andy Yun <andy000923@gmail.com>
2024-04-15 17:36:17 +0900

    fix:[24.04.15]create dockerfile

--- README.md
+++ README.md
@@ -1,19 +1,11 @@
-# Getting Started with Create React App
-
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
 ## Available Scripts
-
 
 ### `npm install`
 
-To install the project.
-In the project directory, you can run:
-
 ### `npm start`
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+## Docker commands
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+### `docker build -t univcert-image .`
+
+### `docker run -d -p 3000:3000 --name univcert-app univcert-image`
--- src/components/ContactUsButton.jsx
+++ src/components/ContactUsButton.jsx
@@ -1,14 +1,13 @@
 import "./ContactUsButton.css";
-import Button9 from "../assets/Button9.svg";
-import image9 from "../assets/image9.svg";
+import github from "../assets/github.svg";
 const ContactUsButton = (props) => {
   return (
     <div className="flex-container-5">
-      <a href="https://github.com/in-seo/univcert">
-        <img className="image9" alt="github" src={image9} />
+      <a href="https://github.com/in-seo/univcert" target="_blank" rel="noopener noreferrer">
+        <img className="github" alt="github" src={github} />
       </a>
-      <a href="https://fork-player-abb.notion.site/63617df354c042c39382d7f2d9a9bab9">
-        <img className="Button9" alt="notion" src={Button9} />
+      <a href="https://fork-player-abb.notion.site/63617df354c042c39382d7f2d9a9bab9" target="_blank" rel="noopener noreferrer">
+        <button className="contact-button">Contact Us</button>
       </a>
     </div>
   );
